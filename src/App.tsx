59bf4e7e4d8414bf09381b61bb9b14e670c0867f
Juanmicheljm65 <juanmicheljm65@gmail.com>
2024-02-01 18:39:36 -0300

    Creating List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Form from "./Components/Form";
+import List from "./Components/List";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <List />
     </div>
   );
 }
